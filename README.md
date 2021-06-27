084171fad13867eb2d69c0f08efe5d6765318baf
Proggit <proggit@mail.ru>
2021-06-27 17:20:51 +0500

    Step  3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/VimLoko/php-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/VimLoko/php-project-lvl2/actions)
+### Difference calculator
+### Flat JSON:
+[![asciicast](https://asciinema.org/a/lphxy4HtG3Nxq2etQNrhMP1hH.svg)](https://asciinema.org/a/lphxy4HtG3Nxq2etQNrhMP1hH)
---
+++ src/GenDiff.php
@@ -0,0 +1,82 @@
+<?php
+namespace Differ\Differ;
+
+/**
+ * @throws \Exception
+ */
+function readFile(string $path): string
+{
+    if (!file_exists($path)) {
+        throw new \Exception("Can't find file in {$path}");
+    }
+    return file_get_contents($path, true);
+}
+
+/**
+ * @throws \JsonException
+ */
+function jsonToArray(string $json): array
+{
+    return json_decode($json, true, 512, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR);
+}
+
+function toString($value)
+{
+    return trim(var_export($value, true), "'");
+}
+
+function mergeArraysKeys(array ...$arrays): array
+{
+    $result = [];
+    foreach ($arrays as $array) {
+        $result = [...$result, ...array_keys($array)];
+    }
+    return array_values(
+        array_unique($result)
+    );
+}
+
+function formating(array $array): string
+{
+    $mergeKeyValue = array_map(
+        function($key, $value) {
+            $key = toString($key);
+            $value = toString($value);
+            return "  {$key}: {$value}";
+        },
+        array_keys($array),
+        $array
+    );
+    $implodeAr = implode("\n", $mergeKeyValue);
+    return "{\n{$implodeAr}\n}\n";
+}
+
+function gendiff(string $firstFile, string $secondFile): string
+{
+    $firstFileContent = jsonToArray(readFile($firstFile));
+    $secondFileContent = jsonToArray(readFile($secondFile));
+    $keys = mergeArraysKeys($firstFileContent, $secondFileContent);
+    sort($keys);
+    $result = [];
+    foreach ($keys as  $key) {
+        $existKeyInFirstAr = array_key_exists($key, $firstFileContent);
+        $existKeyInSecondAr = array_key_exists($key, $secondFileContent);
+        $valueFirstAr = $firstFileContent[$key] ?? null;
+        $valueSecondAr = $secondFileContent[$key] ?? null;
+        if ($existKeyInFirstAr && $existKeyInSecondAr) {
+            if ($valueFirstAr === $valueSecondAr) {
+                $result["  {$key}"] = $valueFirstAr;
+            } else  {
+                $result["- {$key}"] = $valueFirstAr;
+                $result["+ {$key}"] = $valueSecondAr;
+            }
+        }
+        if ($existKeyInFirstAr && !$existKeyInSecondAr) {
+            $result["- {$key}"] = $valueFirstAr;
+        }
+        if (!$existKeyInFirstAr && $existKeyInSecondAr) {
+            $result["+ {$key}"] = $valueSecondAr;
+        }
+    }
+    return formating($result);
+}
